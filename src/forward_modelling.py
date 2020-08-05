bf29e1cb59ea7e7b45879c5ed64fddd4a0bc2ec8
Senthil Palanivelu <senthilcaesar@gmail.com>
2020-08-05 17:55:21 -0400

    pec

--- src/forward_modelling.py
+++ src/forward_modelling.py
@@ -7,6 +7,9 @@ from surfer import Brain
 from IPython.display import Image
 from mayavi import mlab
 from mne.viz import plot_alignment, set_3d_view
+from mne.preprocessing import compute_proj_ecg, compute_proj_eog
+from mne.connectivity import envelope_correlation
+from mne.minimum_norm import make_inverse_operator, apply_inverse_epochs
 import matplotlib.pyplot as plt
 os.environ['ETS_TOOLKIT'] = 'qt4'
 os.environ['QT_API'] = 'pyqt'
@@ -60,11 +63,13 @@ def view_SS_brain(subject, subjects_dir, src):
     mlab.show()
 
 
-def compute_SS(subject, subjects_dir):
+def compute_SourceSpace(subject, subjects_dir, src_fname, plot=True):
     src = mne.setup_source_space(subject, spacing='oct6', add_dist='patch',
                                 subjects_dir=subjects_dir)
-    mne.viz.plot_bem(subject=subject, subjects_dir=subjects_dir,
-                    src=src, orientation='coronal')
+    src.save(src_fname, overwrite=True)
+    if plot:
+        mne.viz.plot_bem(subject=subject, subjects_dir=subjects_dir,
+                        src=src, orientation='coronal')
     return src
 
 
@@ -119,6 +124,19 @@ def sensitivty_plot(subject, subjects_dir, fwd):
     Image(filename=f'sensitivity_map_grad_{view}.jpg', width=400)
 
 
+def view_source_origin(corr, labels, inv, subjects_dir):
+    threshold_prop = 0.15
+    degree = mne.connectivity.degree(corr, threshold_prop=threshold_prop)
+    stc = mne.labels_to_stc(labels, degree)
+    stc = stc.in_label(mne.Label(inv['src'][0]['vertno'], hemi='lh') +
+                    mne.Label(inv['src'][1]['vertno'], hemi='rh'))
+    brain = stc.plot(
+        clim=dict(kind='percent', lims=[75, 85, 95]), colormap='gnuplot',
+        subjects_dir=subjects_dir, views='dorsal', hemi='both',
+        smoothing_steps=25, time_label='Beta band')
+    mlab.show()
+
+
 # cases_meg = '/home/senthil/caesar/camcan/cc700/meg/pipeline/release004/BIDS_20190411/meg_rest_raw/cases.txt'
 # cases_T1 = '/home/senthil/caesar/camcan/cc700/mri/pipeline/release004/BIDS_20190411/anat/cases.txt'
 # with open(cases_meg) as f:
@@ -134,6 +152,8 @@ subject='sub-CC221373'
 subjects_dir='/Users/senthilp/Downloads/tmp'
 fname_meg = f'{subjects_dir}/{subject}_ses-rest_task-rest.fif'
 fwd_fname = f'{subjects_dir}/{subject}-fwd.fif.gz'
+src_fname = f'{subjects_dir}/{subject}-src.fif.gz'
+cov_fname = f'{subjects_dir}/{subject}-cov.fif.gz'
 
 # compute_bem(subject, subjects_dir)
 # compute_scalp_surfaces(subject, subjects_dir)
@@ -144,8 +164,40 @@ trans = f'{subjects_dir}/{subject}-trans.fif'
 info = mne.io.read_info(fname_meg)
 
 # plot_registration(info, trans, subject, subjects_dir)
-# src = compute_SS(subject, subjects_dir)
+# compute_SourceSpace(subject, subjects_dir, src_fname, plot=Falseafari)
+src = mne.read_source_spaces(src_fname)
 # view_SS_brain(subject, subjects_dir, src)
 # forward_model(subject, subjects_dir, fname_meg, trans, src, fwd_fname)
 fwd = mne.read_forward_solution(fwd_fname)
-sensitivty_plot(subject, subjects_dir, fwd)
+#sensitivty_plot(subject, subjects_dir, fwd)
+
+raw = mne.io.read_raw_fif(fname_meg, verbose='error')
+# projs_ecg, _ = compute_proj_ecg(raw, n_grad=1, n_mag=2)
+# projs_eog, _ = compute_proj_eog(raw, n_grad=1, n_mag=2)
+# raw.info['projs'] += projs_ecg
+# raw.info['projs'] += projs_eog
+# raw.apply_proj()
+# cov_arr = mne.compute_raw_covariance(raw)
+# cov_arr.save(cov_fname)
+cov = mne.read_cov(cov_fname)
+events = mne.find_events(raw, stim_channel=['STI201','STI101','STI003'], 
+                        uint_cast=True, initial_event=True, verbose=False)
+epochs = mne.Epochs(raw, events=events,
+                    baseline=None, preload=True, event_repeated='merge')
+inv = make_inverse_operator(epochs.info, fwd, cov)
+
+labels = mne.read_labels_from_annot(subject, 'aparc',
+                                    subjects_dir=subjects_dir)
+epochs.apply_hilbert()  # faster to apply in sensor space
+stcs = apply_inverse_epochs(epochs, inv, lambda2=1. / 9., pick_ori='normal',
+                            return_generator=True)
+label_ts = mne.extract_label_time_course(
+    stcs, labels, inv['src'], return_generator=True)
+corr = envelope_correlation(label_ts, verbose=True)
+
+# let's plot this matrix
+# fig, ax = plt.subplots(figsize=(4, 4))
+# ax.imshow(corr, cmap='viridis', clim=np.percentile(corr, [5, 95]))
+# fig.tight_layout()
+# plt.show()
+view_source_origin(corr, labels, inv, subjects_dir)
