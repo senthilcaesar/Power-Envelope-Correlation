d950c56f93b2173c1d538ea2e1666ea069f4ff0e
senthilcaesar@yahoo.co.in <senthilcaesar@gmail.com>
2020-10-17 12:10:56 -0400

    errobar updates

--- utils/ploti.py
+++ utils/ploti.py
@@ -15,7 +15,6 @@ sensory_mean = {'sc':None, 'ac':None, 'vc':None}
 fig, ax = plt.subplots(figsize=(5, 3))
 x_pnts = np.arange(len(freq))
 
-mean_dict = []
 for label in sensory_mean:
     myDict = {key: [] for key in freq}
     for subject in case_list:
@@ -25,7 +24,6 @@ for label in sensory_mean:
             if Path(corr_data_file).exists():
                 corr_data = float(np.load(corr_data_file))
                 myDict[val].append(corr_data)
-    mean_dict.append(myDict)
     sensory_mean[label] = [np.mean(value) for key, value in myDict.items()]
     
 
