5196bf043bb832877b8708c281a8307a6bd5e127
senthilcaesar@yahoo.co.in <senthilcaesar@gmail.com>
2020-12-15 13:41:25 -0500

    photoscape

---
+++ software.sh
@@ -0,0 +1,5 @@
+sudo dnf install snapd
+sudo snap install photoscape
+sudo snap install gimp
+sudo snap install mailspring
+git clone https://github.com/flazz/vim-colorschemes ~/.vim/
--- src/forward_modelling_serial.py
+++ src/forward_modelling_serial.py
@@ -356,189 +356,203 @@ for freq in freqs:
         corr_true_file_scRight_wholebrain = f'{DATA_DIR}/{subject}_corr_ortho_true_{volume_spacing}_{frequency}_scRight_wholebrain.npy'
         corr_true_file_vcRight_wholebrain = f'{DATA_DIR}/{subject}_corr_ortho_true_{volume_spacing}_{frequency}_vcRight_wholebrain.npy'
 
-        trans = f'/home/senthilp/caesar/camcan/cc700/camcan_coreg-master/trans/{subject}-trans.fif' # The transformation file obtained by coregistration
-        file_trans = pathlib.Path(trans)
-        file_ss = pathlib.Path(src_fname)
-        file_fm = pathlib.Path(fwd_fname)
-        file_proj = pathlib.Path(raw_proj)
-        file_cov = pathlib.Path(cov_fname)
-        isdir_bem = pathlib.Path(bem_check)
 
-        acLeft_file = pathlib.Path(corr_true_file_acLeft_wholebrain)
-        scLeft_file = pathlib.Path(corr_true_file_scLeft_wholebrain)
-        vcLeft_file = pathlib.Path(corr_true_file_vcLeft_wholebrain)
-        acRight_file = pathlib.Path(corr_true_file_acRight_wholebrain)
-        scRight_file = pathlib.Path(corr_true_file_scRight_wholebrain)
-        vcRight_file = pathlib.Path(corr_true_file_vcRight_wholebrain)
+        check_for_files = []
+        check_for_files.append(corr_true_file_acLeft_wholebrain)
+        check_for_files.append(corr_true_file_scLeft_wholebrain)
+        check_for_files.append(corr_true_file_vcLeft_wholebrain)
+        check_for_files.append(corr_true_file_acRight_wholebrain)
+        check_for_files.append(corr_true_file_scRight_wholebrain)
+        check_for_files.append(corr_true_file_vcRight_wholebrain)
 
-        t1 = nib.load(t1_fname)
+        file_exist = [f for f in check_for_files if os.path.isfile(f)]
+        file_not_exist = list(set(file_exist) ^ set(check_for_files))
 
-        if not file_trans.exists():
-            print (f'{trans} File doesnt exist...')
-            sys.exit(0)
+        if len(file_not_exist) > 1:
 
-        info = mne.io.read_info(fname_meg)
-        # plot_registration(info, trans, subject, subjects_dir)
+            trans = f'/home/senthilp/caesar/camcan/cc700/camcan_coreg-master/trans/{subject}-trans.fif' # The transformation file obtained by coregistration
+            file_trans = pathlib.Path(trans)
+            file_ss = pathlib.Path(src_fname)
+            file_fm = pathlib.Path(fwd_fname)
+            file_proj = pathlib.Path(raw_proj)
+            file_cov = pathlib.Path(cov_fname)
+            isdir_bem = pathlib.Path(bem_check)
 
-        if not file_ss.exists():
-            src = compute_SourceSpace(subject, subjects_dir, src_fname, source_voxel_coords, plot=True, ss=space, 
-                                volume_spacing=volume_spacing)
-            seed_l_sc = MNI_to_MRI(subject, subjects_dir, t1, ROI_mni['SSC_Left'])
-            seed_r_sc = MNI_to_MRI(subject, subjects_dir, t1, ROI_mni['SSC_Right'])
-            seed_l_ac = MNI_to_MRI(subject, subjects_dir, t1, ROI_mni['AC_Left'])
-            seed_r_ac = MNI_to_MRI(subject, subjects_dir, t1, ROI_mni['AC_Right'])
-            seed_l_vc = MNI_to_MRI(subject, subjects_dir, t1, ROI_mni['VC_Left'])
-            seed_r_vc = MNI_to_MRI(subject, subjects_dir, t1, ROI_mni['VC_Right'])
-            src_inuse = np.where(src[0]['inuse'] == 1)
-            loc_l_sc = src_inuse[0][0]
-            loc_r_sc = src_inuse[0][1]
-            loc_l_ac = src_inuse[0][2]
-            loc_r_ac = src_inuse[0][3]
-            loc_l_vc = src_inuse[0][4]
-            loc_r_vc = src_inuse[0][5]
-            src[0]['rr'][loc_l_sc] = seed_l_sc
-            src[0]['rr'][loc_r_sc] = seed_r_sc
-            src[0]['rr'][loc_l_ac] = seed_l_ac
-            src[0]['rr'][loc_r_ac] = seed_r_ac
-            src[0]['rr'][loc_l_vc] = seed_l_vc
-            src[0]['rr'][loc_r_vc] = seed_r_vc
-            src.save(src_fname, overwrite=True)
-        src = mne.read_source_spaces(src_fname)
-        #view_SS_brain(subject, subjects_dir, src)
+            acLeft_file = pathlib.Path(corr_true_file_acLeft_wholebrain)
+            scLeft_file = pathlib.Path(corr_true_file_scLeft_wholebrain)
+            vcLeft_file = pathlib.Path(corr_true_file_vcLeft_wholebrain)
+            acRight_file = pathlib.Path(corr_true_file_acRight_wholebrain)
+            scRight_file = pathlib.Path(corr_true_file_scRight_wholebrain)
+            vcRight_file = pathlib.Path(corr_true_file_vcRight_wholebrain)
 
-        if not file_fm.exists():
-            forward_model(subject, subjects_dir, fname_meg, trans, src, fwd_fname)
-        fwd = mne.read_forward_solution(fwd_fname)
+            t1 = nib.load(t1_fname)
 
-        # sensitivty_plot(subject, subjects_dir, fwd)
-        raw = mne.io.read_raw_fif(fname_meg, verbose='error', preload=True)
+            if not file_trans.exists():
+                print (f'{trans} File doesnt exist...')
+                sys.exit(0)
 
-        srate = raw.info['sfreq']
-        n_time_samps = raw.n_times
-        time_secs = raw.times
-        ch_names = raw.ch_names
-        n_chan = len(ch_names)
-        freq_res =  srate/n_time_samps
-        print('\n')
-        print('-------------------------- Data summary-------------------------------')
-        print(f'Subject {subject}')
-        print(f"Frequency resolution {freq_res} Hz")
-        print(f"The first few channel names are {ch_names[:3]}")
-        print(f"The last time sample at {time_secs[-1]} seconds.")
-        print(f"Sampling Frequency (No of time points/sec) {srate} Hz")
-        print(f"Miscellaneous acquisition info {raw.info['description']}")
-        print(f"Bad channels marked during data acquisition {raw.info['bads']}")
-        print(f"Convert time in sec ( 60s ) to ingeter index {raw.time_as_index(60)}") # Convert time to indices
-        print(f"The raw data object has {n_time_samps} time samples and {n_chan} channels.")
-        print('------------------------------------------------------------------------')
-        print('\n')
-        # raw.plot(n_channels=10, scalings='auto', title='Data from arrays', show=True, block=True)
-        if not file_proj.exists():
-            projs_ecg, _ = compute_proj_ecg(raw, n_grad=1, n_mag=2, ch_name='ECG063')
-            projs_eog1, _ = compute_proj_eog(raw, n_grad=1, n_mag=2, ch_name='EOG061')
-            projs_eog2, _ = compute_proj_eog(raw, n_grad=1, n_mag=2, ch_name='EOG062')
-            if projs_ecg is not None:
-                mne.write_proj(heartbeat_proj, projs_ecg) # Saving projectors
-                raw.info['projs'] += projs_ecg
-            if projs_eog1 is not None:
-                mne.write_proj(eye_proj1, projs_eog1)
-                raw.info['projs'] += projs_eog1
-            if projs_eog2 is not None:
-                mne.write_proj(eye_proj2, projs_eog2)
-                raw.info['projs'] += projs_eog2
-            raw.apply_proj()
-            raw.save(raw_proj, proj=True, overwrite=True)
-        raw_proj_applied = mne.io.read_raw_fif(raw_proj, verbose='error', preload=True)
+            info = mne.io.read_info(fname_meg)
+            # plot_registration(info, trans, subject, subjects_dir)
 
-        print(f'High-pass filtering data at 0.5 Hz')
-        raw_proj_applied.filter(l_freq=0.5, h_freq=None, method='iir')
+            if not file_ss.exists():
+                src = compute_SourceSpace(subject, subjects_dir, src_fname, source_voxel_coords, plot=True, ss=space, 
+                                    volume_spacing=volume_spacing)
+                seed_l_sc = MNI_to_MRI(subject, subjects_dir, t1, ROI_mni['SSC_Left'])
+                seed_r_sc = MNI_to_MRI(subject, subjects_dir, t1, ROI_mni['SSC_Right'])
+                seed_l_ac = MNI_to_MRI(subject, subjects_dir, t1, ROI_mni['AC_Left'])
+                seed_r_ac = MNI_to_MRI(subject, subjects_dir, t1, ROI_mni['AC_Right'])
+                seed_l_vc = MNI_to_MRI(subject, subjects_dir, t1, ROI_mni['VC_Left'])
+                seed_r_vc = MNI_to_MRI(subject, subjects_dir, t1, ROI_mni['VC_Right'])
+                src_inuse = np.where(src[0]['inuse'] == 1)
+                loc_l_sc = src_inuse[0][0]
+                loc_r_sc = src_inuse[0][1]
+                loc_l_ac = src_inuse[0][2]
+                loc_r_ac = src_inuse[0][3]
+                loc_l_vc = src_inuse[0][4]
+                loc_r_vc = src_inuse[0][5]
+                src[0]['rr'][loc_l_sc] = seed_l_sc
+                src[0]['rr'][loc_r_sc] = seed_r_sc
+                src[0]['rr'][loc_l_ac] = seed_l_ac
+                src[0]['rr'][loc_r_ac] = seed_r_ac
+                src[0]['rr'][loc_l_vc] = seed_l_vc
+                src[0]['rr'][loc_r_vc] = seed_r_vc
+                src.save(src_fname, overwrite=True)
+            src = mne.read_source_spaces(src_fname)
+            #view_SS_brain(subject, subjects_dir, src)
 
-        if not file_cov.exists():
-            cov = mne.compute_raw_covariance(raw_proj_applied) # compute before band-pass of interest
-            mne.write_cov(cov_fname, cov)
-        cov = mne.read_cov(cov_fname) 
+            if not file_fm.exists():
+                forward_model(subject, subjects_dir, fname_meg, trans, src, fwd_fname)
+            fwd = mne.read_forward_solution(fwd_fname)
 
-        # cov.plot(raw.info, proj=True, exclude='bads', show_svd=False
-        # raw_proj_applied.crop(tmax=10)
+            # sensitivty_plot(subject, subjects_dir, fwd)
+            raw = mne.io.read_raw_fif(fname_meg, verbose='error', preload=True)
+
+            srate = raw.info['sfreq']
+            n_time_samps = raw.n_times
+            time_secs = raw.times
+            ch_names = raw.ch_names
+            n_chan = len(ch_names)
+            freq_res =  srate/n_time_samps
+            print('\n')
+            print('-------------------------- Data summary-------------------------------')
+            print(f'Subject {subject}')
+            print(f"Frequency resolution {freq_res} Hz")
+            print(f"The first few channel names are {ch_names[:3]}")
+            print(f"The last time sample at {time_secs[-1]} seconds.")
+            print(f"Sampling Frequency (No of time points/sec) {srate} Hz")
+            print(f"Miscellaneous acquisition info {raw.info['description']}")
+            print(f"Bad channels marked during data acquisition {raw.info['bads']}")
+            print(f"Convert time in sec ( 60s ) to ingeter index {raw.time_as_index(60)}") # Convert time to indices
+            print(f"The raw data object has {n_time_samps} time samples and {n_chan} channels.")
+            print('------------------------------------------------------------------------')
+            print('\n')
+            # raw.plot(n_channels=10, scalings='auto', title='Data from arrays', show=True, block=True)
+            if not file_proj.exists():
+                projs_ecg, _ = compute_proj_ecg(raw, n_grad=1, n_mag=2, ch_name='ECG063')
+                projs_eog1, _ = compute_proj_eog(raw, n_grad=1, n_mag=2, ch_name='EOG061')
+                projs_eog2, _ = compute_proj_eog(raw, n_grad=1, n_mag=2, ch_name='EOG062')
+                if projs_ecg is not None:
+                    mne.write_proj(heartbeat_proj, projs_ecg) # Saving projectors
+                    raw.info['projs'] += projs_ecg
+                if projs_eog1 is not None:
+                    mne.write_proj(eye_proj1, projs_eog1)
+                    raw.info['projs'] += projs_eog1
+                if projs_eog2 is not None:
+                    mne.write_proj(eye_proj2, projs_eog2)
+                    raw.info['projs'] += projs_eog2
+                raw.apply_proj()
+                raw.save(raw_proj, proj=True, overwrite=True)
+            raw_proj_applied = mne.io.read_raw_fif(raw_proj, verbose='error', preload=True)
+
+            print(f'High-pass filtering data at 0.5 Hz')
+            raw_proj_applied.filter(l_freq=0.5, h_freq=None, method='iir')
+
+            if not file_cov.exists():
+                cov = mne.compute_raw_covariance(raw_proj_applied) # compute before band-pass of interest
+                mne.write_cov(cov_fname, cov)
+            cov = mne.read_cov(cov_fname) 
+
+            # cov.plot(raw.info, proj=True, exclude='bads', show_svd=False
+            # raw_proj_applied.crop(tmax=10)
+            
+            do_epochs = False
+
+            l_freq = freq-2.0
+            h_freq = freq+2.0
+            print(f'Band pass filter data [{l_freq}, {h_freq}]')
+            raw_proj_filtered = raw_proj_applied.filter(l_freq=l_freq, h_freq=h_freq)
+
+            if do_epochs:
+                print('Segmenting raw data...')
+                events = mne.make_fixed_length_events(raw_proj_filtered, duration=5.)
+                raw_proj_filtered = mne.Epochs(raw_proj_filtered, events=events, tmin=0, tmax=5.,
+                                                baseline=None, preload=True)
+                data_cov = mne.compute_covariance(raw_proj_filtered)         
+            else:
+                data_cov = mne.compute_raw_covariance(raw_proj_filtered)
+
+
+            seed_left_sc = 0
+            seed_right_sc = 1
+            seed_left_ac = 2
+            seed_right_ac = 3
+            seed_left_vc = 4
+            seed_right_vc = 5
+            
+
+            filters = make_lcmv(raw_proj_filtered.info, fwd, data_cov, 0.05, cov,
+                            pick_ori='max-power', weight_norm='nai')
+            raw_proj_filtered_comp = raw_proj_filtered.apply_hilbert(n_jobs=8)
+
+            if do_epochs:
+                stcs = apply_lcmv_epochs(raw_proj_filtered_comp, filters, return_generator=False)
+            else:
+                stcs = apply_lcmv_raw(raw_proj_filtered_comp, filters, verbose=True)
+                stcs = [stcs]
+            
+            # Power Envelope Correlation
+            print(f'Computing Power Envelope Correlation for {subject}....Orthogonalize True')
         
-        do_epochs = False
+            # Left seed to whole brain
+            if not scLeft_file.exists():
+                corr_true_scLeft = envelope_correlation(stcs, seed=seed_left_sc)
+                np.save(corr_true_file_scLeft_wholebrain, corr_true_scLeft)
+                del corr_true_scLeft
+            else:
+            print(f'File exsist {corr_true_file_scLeft_wholebrain}')
+            if not acLeft_file.exists():
+                corr_true_acLeft = envelope_correlation(stcs,seed=seed_left_ac)
+                np.save(corr_true_file_acLeft_wholebrain, corr_true_acLeft)
+                del corr_true_acLeft
+            else:
+                print(f'File exsist {corr_true_file_acLeft_wholebrain}')
+            if not vcLeft_file.exists():
+                corr_true_vcLeft = envelope_correlation(stcs,seed=seed_left_vc)
+                np.save(corr_true_file_vcLeft_wholebrain, corr_true_vcLeft)
+                del corr_true_vcLeft
+            else:
+                print(f'File exsist {corr_true_file_vcLeft_wholebrain}')
 
-        l_freq = freq-2.0
-        h_freq = freq+2.0
-        print(f'Band pass filter data [{l_freq}, {h_freq}]')
-        raw_proj_filtered = raw_proj_applied.filter(l_freq=l_freq, h_freq=h_freq)
-
-        if do_epochs:
-            print('Segmenting raw data...')
-            events = mne.make_fixed_length_events(raw_proj_filtered, duration=5.)
-            raw_proj_filtered = mne.Epochs(raw_proj_filtered, events=events, tmin=0, tmax=5.,
-                                            baseline=None, preload=True)
-            data_cov = mne.compute_covariance(raw_proj_filtered)         
-        else:
-            data_cov = mne.compute_raw_covariance(raw_proj_filtered)
-
-
-        seed_left_sc = 0
-        seed_right_sc = 1
-        seed_left_ac = 2
-        seed_right_ac = 3
-        seed_left_vc = 4
-        seed_right_vc = 5
-        
-
-        filters = make_lcmv(raw_proj_filtered.info, fwd, data_cov, 0.05, cov,
-                        pick_ori='max-power', weight_norm='nai')
-        raw_proj_filtered_comp = raw_proj_filtered.apply_hilbert(n_jobs=8)
-
-        if do_epochs:
-            stcs = apply_lcmv_epochs(raw_proj_filtered_comp, filters, return_generator=False)
-        else:
-            stcs = apply_lcmv_raw(raw_proj_filtered_comp, filters, verbose=True)
-            stcs = [stcs]
-        
-        # Power Envelope Correlation
-        print(f'Computing Power Envelope Correlation for {subject}....Orthogonalize True')
-       
-        # Left seed to whole brain
-        if not scLeft_file.exists():
-            corr_true_scLeft = envelope_correlation(stcs, seed=seed_left_sc)
-            np.save(corr_true_file_scLeft_wholebrain, corr_true_scLeft)
-            del corr_true_scLeft
-        else:
-           print(f'File exsist {corr_true_file_scLeft_wholebrain}')
-        if not acLeft_file.exists():
-            corr_true_acLeft = envelope_correlation(stcs,seed=seed_left_ac)
-            np.save(corr_true_file_acLeft_wholebrain, corr_true_acLeft)
-            del corr_true_acLeft
-        else:
-            print(f'File exsist {corr_true_file_acLeft_wholebrain}')
-        if not vcLeft_file.exists():
-            corr_true_vcLeft = envelope_correlation(stcs,seed=seed_left_vc)
-            np.save(corr_true_file_vcLeft_wholebrain, corr_true_vcLeft)
-            del corr_true_vcLeft
-        else:
-            print(f'File exsist {corr_true_file_vcLeft_wholebrain}')
-
-        # Right seed to whole brain
-        if not scRight_file.exists():
-            corr_true_scRight = envelope_correlation(stcs,seed=seed_right_sc)
-            np.save(corr_true_file_scRight_wholebrain, corr_true_scRight)
-            del corr_true_scRight
-        else:
-            print(f'File exsist {corr_true_file_scRight_wholebrain}')
-        if not acRight_file.exists():
-            corr_true_acRight = envelope_correlation(stcs,seed=seed_right_ac)
-            np.save(corr_true_file_acRight_wholebrain, corr_true_acRight)
-            del corr_true_acRight
-        else:
-            print(f'File exsist {corr_true_file_acRight_wholebrain}')
-        if not vcRight_file.exists():
-            corr_true_vcRight = envelope_correlation(stcs,seed=seed_right_vc)
-            np.save(corr_true_file_vcRight_wholebrain, corr_true_vcRight)
-            del corr_true_vcRight
-        else:
-            print(f'File exsist {corr_true_file_vcRight_wholebrain}')
-        del stcs
+            # Right seed to whole brain
+            if not scRight_file.exists():
+                corr_true_scRight = envelope_correlation(stcs,seed=seed_right_sc)
+                np.save(corr_true_file_scRight_wholebrain, corr_true_scRight)
+                del corr_true_scRight
+            else:
+                print(f'File exsist {corr_true_file_scRight_wholebrain}')
+            if not acRight_file.exists():
+                corr_true_acRight = envelope_correlation(stcs,seed=seed_right_ac)
+                np.save(corr_true_file_acRight_wholebrain, corr_true_acRight)
+                del corr_true_acRight
+            else:
+                print(f'File exsist {corr_true_file_acRight_wholebrain}')
+            if not vcRight_file.exists():
+                corr_true_vcRight = envelope_correlation(stcs,seed=seed_right_vc)
+                np.save(corr_true_file_vcRight_wholebrain, corr_true_vcRight)
+                del corr_true_vcRight
+            else:
+                print(f'File exsist {corr_true_file_vcRight_wholebrain}')
+            del stcs
 
     time_elapsed = datetime.now() - start_t
     print ('Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed))
