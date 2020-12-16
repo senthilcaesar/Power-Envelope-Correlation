900831d107186d50cfb564d433ae6872e0ad60ce
Senthil Palanivelu <senthilcaesar@yahoo.co.in>
2020-12-16 16:09:18 -0500

    Update software.sh

--- software.sh
+++ software.sh
@@ -4,3 +4,11 @@ sudo snap install gimp
 sudo snap install mailspring
 git clone https://github.com/flazz/vim-colorschemes ~/.vim/
 plasmashell 5.20.4
+
+
+
+os.environ["OMP_NUM_THREADS"] = "10"         # export OMP_NUM_THREADS=1
+os.environ["OPENBLAS_NUM_THREADS"] = "10"    # export OPENBLAS_NUM_THREADS=1
+os.environ["MKL_NUM_THREADS"] = "10"         # export MKL_NUM_THREADS=1
+os.environ["VECLIB_MAXIMUM_THREADS"] = "10"  # export VECLIB_MAXIMUM_THREADS=1
+os.environ["NUMEXPR_NUM_THREADS"] = "10"     # export NUMEXPR_NUM_THREADS=1
