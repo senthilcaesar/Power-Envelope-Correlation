bcf60f6844fa65bee45c8cd1da00e184ba60a4f0
Senthil Palanivelu <senthilcaesar@yahoo.co.in>
2022-06-14 11:19:45 -0400

    Update plot_surf.sh

--- wen/plot_surf.sh
+++ wen/plot_surf.sh
@@ -5,6 +5,7 @@ min='0.0'
 mid='0.17'
 max='0.37'
 name="cluster"
+freeSurfer_dir='/Applications/freesurfer/7.1.1/bin'
 
 for i in ${freqs[@]}; do
 
@@ -15,18 +16,18 @@ for i in ${freqs[@]}; do
     lh_inflated="${sub_dir}/lh.${name}_mapped_array_freq${freq}.avgMapping_allSub_RF_ANTs_MNI152_orig_to_fsaverage.nii.gz"
     rh_inflated="${sub_dir}/rh.${name}_mapped_array_freq${freq}.avgMapping_allSub_RF_ANTs_MNI152_orig_to_fsaverage.nii.gz"
 
-    /Applications/freesurfer/7.1.1/bin/freeview -f ${surf_dir}/lh.inflated:overlay=${lh_inflated}:overlay_threshold=${min},${mid},${max}:overlay_color='colorwheel','inverse' \
+    ${freeSurfer_dir}/freeview -f ${surf_dir}/lh.inflated:overlay=${lh_inflated}:overlay_threshold=${min},${mid},${max}:overlay_color='colorwheel','inverse' \
     --camera azimuth 2 elevation 11 roll -17 --zoom 1.2 --screenshot "${freq}lh1.jpg"
-    /Applications/freesurfer/7.1.1/bin/freeview -f ${surf_dir}/lh.inflated:overlay=${lh_inflated}:overlay_threshold=${min},${mid},${max}:overlay_color='colorwheel','inverse' \
+    ${freeSurfer_dir}/freeview -f ${surf_dir}/lh.inflated:overlay=${lh_inflated}:overlay_threshold=${min},${mid},${max}:overlay_color='colorwheel','inverse' \
     --camera azimuth 180 elevation 0 roll 14 --zoom 1.2 --screenshot "${freq}lh2.jpg"
-    /Applications/freesurfer/7.1.1/bin/freeview -f ${surf_dir}/lh.inflated:overlay=${lh_inflated}:overlay_threshold=${min},${mid},${max}:overlay_color='colorwheel','inverse' \
+    ${freeSurfer_dir}/freeview -f ${surf_dir}/lh.inflated:overlay=${lh_inflated}:overlay_threshold=${min},${mid},${max}:overlay_color='colorwheel','inverse' \
     --camera azimuth 90 elevation 90 roll 0 --zoom 1.2 --screenshot "${freq}lh3.jpg"
 
-    /Applications/freesurfer/7.1.1/bin/freeview -f ${surf_dir}/rh.inflated:overlay=${rh_inflated}:overlay_threshold=${min},${mid},${max}:overlay_color='colorwheel','inverse' \
+    ${freeSurfer_dir}/freeview -f ${surf_dir}/rh.inflated:overlay=${rh_inflated}:overlay_threshold=${min},${mid},${max}:overlay_color='colorwheel','inverse' \
     --camera azimuth 2 elevation 11 roll -17 --zoom 1.2 --screenshot "${freq}rh1.jpg"
-    /Applications/freesurfer/7.1.1/bin/freeview -f ${surf_dir}/rh.inflated:overlay=${rh_inflated}:overlay_threshold=${min},${mid},${max}:overlay_color='colorwheel','inverse' \
+    ${freeSurfer_dir}/freeview -f ${surf_dir}/rh.inflated:overlay=${rh_inflated}:overlay_threshold=${min},${mid},${max}:overlay_color='colorwheel','inverse' \
     --camera azimuth 180 elevation 0 roll 14 --zoom 1.2 --screenshot "${freq}rh2.jpg"
-    /Applications/freesurfer/7.1.1/bin/freeview -f ${surf_dir}/rh.inflated:overlay=${rh_inflated}:overlay_threshold=${min},${mid},${max}:overlay_color='colorwheel','inverse' \
+    ${freeSurfer_dir}/freeview -f ${surf_dir}/rh.inflated:overlay=${rh_inflated}:overlay_threshold=${min},${mid},${max}:overlay_color='colorwheel','inverse' \
     --camera azimuth 90 elevation 90 roll 0 --zoom 1.2 --screenshot "${freq}rh3.jpg"
 
 done
